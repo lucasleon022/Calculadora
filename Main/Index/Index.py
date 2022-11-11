8784a6b249639dfdb0051db23a916e0cd5672678
lucasleon022 <115665050+lucasleon022@users.noreply.github.com>
2022-11-11 18:47:40 -0300

    Cambio de valores y salida

--- Main/Index/Index.py
+++ Main/Index/Index.py
@@ -7,38 +7,43 @@
 # Developer: Lucas Leon
 ###########################################
 
-number = int(input("ingresa un numero"))
-numberr = int(input("ingresa otro numero"))
+number = int(input("Ingresa un numero"))
+numberr = int(input("Ingresa otro numero"))
 
 eleccion = 0
 
 while eleccion != 6:
     print("""
-    indique la operacion a realizar:
-    1) suma
-    2) resta
-    3) multiplicacion
-    4) division
-    5) cambio de valores
-    6) salir
+    Indique la operacion a realizar:
+    1) Suma
+    2) Resta
+    3) Multiplicacion
+    4) Division
+    5) Cambio de valores
+    6) Salir
     """)
 
     eleccion = int(input())
 
     if eleccion == 1:
         print(" ")
-        print("resultado: ", number, "+", numberr, "=", number+numberr)
+        print("Resultado: ", number, "+", numberr, "=", number+numberr)
 
     if eleccion == 2:
         print(" ")
-        print("resultado: ", number, "-", numberr, "=", number-numberr)
+        print("Resultado: ", number, "-", numberr, "=", number-numberr)
 
     if eleccion == 3:
         print(" ")
-        print("resultado: ", number, "*", numberr, "=", number*numberr)
+        print("Resultado: ", number, "*", numberr, "=", number*numberr)
 
     if eleccion == 4:
         print(" ")
-        print("resultado: ", number, "/", numberr, "=", number/numberr)
+        print("Resultado: ", number, "/", numberr, "=", number/numberr)
 
-    
+    if eleccion == 5:
+        number = int(input("Ingresa un numero"))
+        numberr = int(input("Ingresa otro numero"))
+
+    if eleccion == 6:
+        print("Gracias por usar la calculadora. Creada por: Lucas Leon")
