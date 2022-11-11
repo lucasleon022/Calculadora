a4899e490ef5d57e39c8b7ef2a5397a56e831d9c
lucasleon022 <115665050+lucasleon022@users.noreply.github.com>
2022-11-11 17:29:18 -0300

    Create Index.py

---
+++ Main/Index/Index.py
@@ -0,0 +1,8 @@
+############################################
+# Primer commit main Calculadora
+# *Subir a Github
+# *Index.py
+# Ejecutable.exe
+# Release: 11/11/2022
+# Developer: Lucas Leon
+###########################################
