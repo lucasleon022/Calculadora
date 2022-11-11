37359763799cd1e82884f27fda3812f8f897b31e
lucasleon022 <115665050+lucasleon022@users.noreply.github.com>
2022-11-11 18:20:31 -0300

    suma

--- Main/Index/Index.py
+++ Main/Index/Index.py
@@ -6,3 +6,25 @@
 # Release: 11/11/2022
 # Developer: Lucas Leon
 ###########################################
+
+number = int(input("ingresa un numero"))
+numberr = int(input("ingresa otro numero"))
+
+eleccion = 0
+
+while eleccion != 6:
+    print("""
+    indique la operacion a realizar:
+    1) suma
+    2) resta
+    3) multiplicacion
+    4) divicion
+    5) cambio de valores
+    6) salir
+    """)
+
+    eleccion = int(input())
+
+    if eleccion == 1:
+        print(" ")
+        print("resultado: ", number, "+", numberr, "=", number+numberr)
